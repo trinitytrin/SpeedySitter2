98c6389173471d4d5be03fc6ab63d5b40a440c65
trinitytrin <choudhury.trina@gmail.com>
2016-10-26 18:35:30 -0500

    For Azure

---
+++ Service.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.Adapters;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.WebControls.Expressions;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Configuration;
+using System.Windows; 
+
+public partial class Service : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            CheckMysqlConnection();
+        }
+    }
+
+    private void CheckMysqlConnection()
+    {
+        try
+        {
+            string connectionString = @"Data Source=localhost; Database=speedy_sitter; User ID=Trina; Password=123";
+            using (MySqlConnection cn = new MySqlConnection(connectionString))
+            {
+                cn.Open();
+                //Response.Write("MySQL connection successful!");
+                using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM address"))
+                {
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter())
+                    {
+                        cmd.Connection = cn;
+                        sda.SelectCommand = cmd;
+
+                        DataTable dt = new DataTable();
+                        
+                            sda.Fill(dt);
+                        //GridView.DataSource = dt;
+                        //GridView.DataBind(dt);
+
+
+                           // GridView.DisabledCssClass
+                        
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write("Exception caught in Service page connection to database: "+ ex);
+      
+        }
+    }
+
+ 
+}
